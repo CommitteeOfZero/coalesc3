1f2e0fb1b42b57e98352ac7a657c3021dcea0bdc
fl4tisjustice <chaoticflat@gmail.com>
2025-10-19 21:49:08 +0100

    Temp fix, come back to this later

--- build.py
+++ build.py
@@ -106,7 +106,7 @@ def main() -> None:
 	load_patches(data_dir / build_info.game / f"patches_{ build_info.platform }{ lang_suffix }")
 
 	if build_info.selected != Language.JAPANESE or build_info.selected == "all":
-		txt_dir = data_dir / build_info.game / f"txt_{ build_info.selected }"
+		txt_dir = data_dir / build_info.game / f"txt_{ build_info.selected if build_info.selected != "all" else "eng" }"
 		TranslationProcessor(
 			patcher,
 			"10_translation/",
